496dd2a540a5690620e3c0f89afde8bc76c91750
Eric Fang <thericfang@10-16-90-60.dynapool.wireless.nyu.edu>
2018-10-11 14:32:31 -0400

    test

---
+++ Process.java
@@ -0,0 +1,32 @@
+public class Process {
+    int a, b, c, m;
+    public Process(int a, int b, int c, int m) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.m = m;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
+    public int getC() {
+        return c;
+    }
+    public int getM() {
+        return m;
+    }
+    public int finishingTime() { // TODO: finishing time of process
+
+    }
+
+    public int turnaroundTime() { // TODO: turnaround time of process = finishing time - A
+
+    }
+
+    public int iOTime() { //
+
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+public class Tester {
+    static ArrayList<ArrayList<Integer>> processes = new ArrayList<ArrayList<Integer>>();
+    static int numOfProcesses;
+    public static void main(String args[]) {
+        System.out.println("What is the file name?");
+        Scanner kbScanner = new Scanner(System.in); // New Scanner to get file name
+        String inputName = kbScanner.nextLine();
+        fileReader(inputName);
+        System.out.println(processes);
+    }
+
+    public static void fileReader(String inputName) { // New Scanner for file input
+        try {
+            Scanner fileInput = new Scanner(new FileInputStream(inputName)).useDelimiter("\\D+");
+            numOfProcesses = fileInput.nextInt();
+            while (fileInput.hasNext()) {   
+                if (fileInput.hasNextInt()) {
+                    for (int i = 0; i < numOfProcesses; i++) {
+                        ArrayList<Integer> temp = new ArrayList<Integer>();
+                        for (int j = 0; j < 4; j++) {
+                            temp.add(fileInput.nextInt());
+                        }
+                        processes.add(temp);          
+                    }
+                    
+                }
+
+                else {
+                    fileInput.next();
+                }
+               
+            }
+            fileInput.close(); 
+            
+        }
+        catch (Exception ex) {
+            // System.out.println("Error reading file " + inputName);
+            System.out.println(ex);
+            System.exit(0);
+        }
+        
+    }
+
+}
