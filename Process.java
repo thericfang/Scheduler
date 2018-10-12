6326e2191b867ba2645db79ac9d7d92f7440268f
Eric Fang <thericfang@10-19-65-101.dynapool.wireless.nyu.edu>
2018-10-12 16:40:35 -0400

    help

--- Process.java
+++ Process.java
@@ -1,23 +1,27 @@
 public class Process {
     int a, b, c, m;
+    int timeLeft;
+    int currentCPUBurst, currentIOBurst;
+    boolean ready, blocked, running;
 
     public Process(int a, int b, int c, int m) {
         this.a = a;
         this.b = b;
         this.c = c;
         this.m = m;
+        this.timeLeft = a;
     }
     public int getA() {
-        return a;
+        return this.a;
     }
     public int getB() {
-        return b;
+        return this.b;
     }
     public int getC() {
-        return c;
+        return this.c;
     }
     public int getM() {
-        return m;
+        return this.m;
     }
     
     // public double finishingTime() { // TODO: finishing time of process
@@ -35,6 +39,29 @@ public class Process {
     // public double waitingTime() { //TODO: time in waiting state
 
     // } 
+
+    public int CPUBurstTime() {
+        this.currentCPUBurst = randomOS(b);
+        timeLeft -= currentCPUBurst;
+        running = true;
+        blocked = false;
+        ready = false;
+        
+    }
+
+    public int IOBurstTime() {
+        this.currentIOBurst = currentCPUBurst * this.m;
+        timeLeft -= currentIOBurst;
+        blocked = true;
+        blocked = false;
+        running = false;
+    }
+
+    public int randomOS(int u) {
+        int random = (int)(Math.random()*Tester.randomNumList.size());
+        System.out.println("random: "+random);
+        return 1 + (Tester.randomNumList.get(random)%u);
+    }
 
     public String toString() {
         return "Process Values: A:" + a + "    B:" + b + "    C:" + c + "     M:" + m;
--- Tester.java
+++ Tester.java
@@ -3,18 +3,26 @@ import java.io.*;
 import java.util.*;
 
 public class Tester {
-    static int numOfProcesses;
-    static ArrayList<Process> processes = new ArrayList<Process>();
+    public static int numOfProcesses; 
+    static ArrayList<Process> arrivingProcesses = new ArrayList<Process>(); // arriving processes
+    static ArrayList<Process> readyProcesses = new ArrayList<Process>(); // ready procesess
+    static ArrayList<Process> blockedProcesses = new ArrayList<Process>(); // blocked processess
+    static ArrayList<Process> terminatedProcesses = new ArrayList<Process>(); // terminated processes
+    Process RunningProcess; // current running process
+    public static ArrayList<Integer> randomNumList = new ArrayList<Integer>(); 
+    public static ArrayList<Integer> sortedProcesses = new ArrayList<Process>(); 
+    public static int time;
     
     public static void main(String args[]) {
         System.out.println("What is the file name?");
         Scanner kbScanner = new Scanner(System.in); // New Scanner to get file name
         String inputName = kbScanner.nextLine();
         fileReader(inputName);
+        randomNumbersReader();
+        System.out.println(processes.get(0).randomOS(processes.get(0).getB()));
         for (int i = 0; i < processes.size(); i++) {
             System.out.println(processes.get(i));
         }
-        
     }
 
     public static void fileReader(String inputName) { // New Scanner for file input
@@ -55,4 +63,45 @@ public class Tester {
         
     }
 
+    public static void randomNumbersReader () {
+        try {
+            Scanner randomNumReader = new Scanner(new FileInputStream("random-numbers"));
+            while (randomNumReader.hasNextInt()) {
+                randomNumList.add(randomNumReader.nextInt());
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void callScheduler(int i) { //while processes still remain
+        while (terminatedProcesses.size() == numOfProcesses) {
+            doBlockedProcesses(i);
+            doRunningProcesses(i);
+            doArrivingProcesses(i);
+            doReadyProcesses(i);
+            time++;
+        }
+    }
+
+    public static void doReadyProcesses(int i) {
+
+    }
+
+    public static void doRunningProcesses(int i) {
+
+    }
+
+    public static void doBlockedProcesses(int i) { //check if out_of_block == current_time where out_of_block = current_time + io_burst 
+        
+    }
+
+    public static void doArrivingProcesses(int i) {
+        for (Process p : arrivingProcesses) {
+            if (p.a == time) {
+                readyProcesses.add(p);
+            }
+        }
+    }
 }
