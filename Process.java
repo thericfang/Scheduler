290e5e05dafb9ef61e11d5741a53dabef79f2f2c
Eric Fang <thericfang@10-19-65-101.dynapool.wireless.nyu.edu>
2018-10-19 19:56:20 -0400

    edits

--- Process.java
+++ Process.java
@@ -1,15 +1,31 @@
 public class Process {
     int a, b, c, m;
-    int timeLeft;
-    int currentCPUBurst, currentIOBurst;
+    int remainingTime;
+    int timeOfNext;
+    int CPUBurst, IOBurst;
+    int remainingBurstTime;
+    int totalIOTime;
     boolean ready, blocked, running;
+    boolean terminated;
+    int outOfBlocked;
+    int outOfRunning;
+    int currentState;
+    String transitionState;
+    String state;
+    int finishingTime; 
+    int turnaroundTime = finishingTime - a;
+    int waitingTime;
+    int priority;
+    int arrivalTimeToReady;
 
     public Process(int a, int b, int c, int m) {
         this.a = a;
         this.b = b;
         this.c = c;
         this.m = m;
-        this.timeLeft = a;
+        this.remainingTime = c;
+        this.totalIOTime = 0;
+        this.state = "Unstarted";
     }
     public int getA() {
         return this.a;
@@ -24,46 +40,34 @@ public class Process {
         return this.m;
     }
     
-    // public double finishingTime() { // TODO: finishing time of process
 
-    // }
-
-    // public double turnaroundTime() { // TODO: turnaround time of process = finishing time - A
-
-    // }
-
-    // public double iOTime() { // TODO: compute I/O time -> time in blocked state
-
-    // }
-
-    // public double waitingTime() { //TODO: time in waiting state
-
-    // } 
-
-    public int CPUBurstTime() {
-        this.currentCPUBurst = randomOS(b);
-        timeLeft -= currentCPUBurst;
-        running = true;
-        blocked = false;
-        ready = false;
+    // public int CPUBurstTime() {
+    //     this.currentCPUBurst = randomOS(b);
+    //     timeLeft -= currentCPUBurst;
         
+    // }
+    
+    public void addCPUBurstTime() {
+        CPUBurst = Tester.randomOS(this.b);
+        outOfRunning = Tester.time + CPUBurst;
+        outOfBlocked = outOfRunning + CPUBurst * m;
     }
 
-    public int IOBurstTime() {
-        this.currentIOBurst = currentCPUBurst * this.m;
-        timeLeft -= currentIOBurst;
-        blocked = true;
-        blocked = false;
-        running = false;
-    }
+    // public int IOBurstTime() {
+    //     this.currentIOBurst = currentCPUBurst * this.m;
+    //     timeLeft -= currentIOBurst;
+    //     blocked = true;
+    //     blocked = false;
+    //     running = false;
+    // }
 
-    public int randomOS(int u) {
-        int random = (int)(Math.random()*Tester.randomNumList.size());
-        System.out.println("random: "+random);
-        return 1 + (Tester.randomNumList.get(random)%u);
-    }
+    
 
     public String toString() {
         return "Process Values: A:" + a + "    B:" + b + "    C:" + c + "     M:" + m;
+    }
+
+    public int compareTo(Process p) {
+        return(p.a-this.a);
     }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 import java.util.*;
+import java.util.Collections;
 
 public class Tester {
     public static int numOfProcesses; 
@@ -8,10 +9,15 @@ public class Tester {
     static ArrayList<Process> readyProcesses = new ArrayList<Process>(); // ready procesess
     static ArrayList<Process> blockedProcesses = new ArrayList<Process>(); // blocked processess
     static ArrayList<Process> terminatedProcesses = new ArrayList<Process>(); // terminated processes
-    Process RunningProcess; // current running process
+    static Process RunningProcess; // current running processes
     public static ArrayList<Integer> randomNumList = new ArrayList<Integer>(); 
-    public static ArrayList<Integer> sortedProcesses = new ArrayList<Process>(); 
     public static int time;
+    static int randomCounter = 0;
+    static int removedFromBlockCounter = 0;
+    // static Comparator<Process> comparator = new MyComparator();
+    // static PriorityQueue<Process> readyProcesses = new PriorityQueue<Process>(comparator);
+    
+   
     
     public static void main(String args[]) {
         System.out.println("What is the file name?");
@@ -19,10 +25,38 @@ public class Tester {
         String inputName = kbScanner.nextLine();
         fileReader(inputName);
         randomNumbersReader();
-        System.out.println(processes.get(0).randomOS(processes.get(0).getB()));
-        for (int i = 0; i < processes.size(); i++) {
-            System.out.println(processes.get(i));
+        // sortList(arrivingProcesses);
+        Collections.sort(arrivingProcesses, 
+            (o1, o2) -> o2.compareTo(o1));  
+        Collections.sort(readyProcesses, 
+            (o1, o2) -> o2.compareTo(o1));  
+        for (int i = 0; i < arrivingProcesses.size(); i++) {
+            arrivingProcesses.get(i).priority++;
+            if (i == 0) {
+                ;
+            }
+            else if (arrivingProcesses.get(i).a == arrivingProcesses.get(i-1).a) {
+                arrivingProcesses.get(i).priority = arrivingProcesses.get(i).priority+1;
+            }
         }
+                
+        callScheduler(1);
+        Collections.sort(terminatedProcesses, 
+                    (o1, o2) -> o2.compareTo(o1));
+        
+        for (int j = 0; j < arrivingProcesses.size(); j++) {
+            Process temp = arrivingProcesses.get(j);
+            System.out.println("Process " + j);
+            System.out.println("        (A,B,C,M) = (" + temp.a + ", " + temp.b + ", " + temp.c + ", " + temp.m + ")" );
+            System.out.println("        Finishing time: " + temp.finishingTime);
+            System.out.println("        Turnaround time: " + (temp.finishingTime - temp.a));
+            System.out.println("        I/O time: " + temp.totalIOTime);
+            System.out.println("        Waiting time: " + temp.waitingTime);
+            // System.out.println(temp + "   Finishing time: " + temp.finishingTime + " IO Time: " + temp.totalIOTime + " Waiting Time: " + temp.waitingTime);
+
+        }
+        System.out.println("Size of arrivingProcesses: " + arrivingProcesses.size());
+        
     }
 
     public static void fileReader(String inputName) { // New Scanner for file input
@@ -42,7 +76,7 @@ public class Tester {
                         c = temp.get(2);
                         m = temp.get(3);
                         Process curProcess = new Process(a,b,c,m);
-                        processes.add(curProcess);          
+                        arrivingProcesses.add(curProcess);          
                     }
                     
                 }
@@ -63,7 +97,7 @@ public class Tester {
         
     }
 
-    public static void randomNumbersReader () {
+    public static void randomNumbersReader() {
         try {
             Scanner randomNumReader = new Scanner(new FileInputStream("random-numbers"));
             while (randomNumReader.hasNextInt()) {
@@ -76,32 +110,208 @@ public class Tester {
     }
 
     public static void callScheduler(int i) { //while processes still remain
-        while (terminatedProcesses.size() == numOfProcesses) {
-            doBlockedProcesses(i);
-            doRunningProcesses(i);
-            doArrivingProcesses(i);
-            doReadyProcesses(i);
-            time++;
+        switch (i) {
+            case 1: {
+                
+                
+                
+                while (terminatedProcesses.size() != arrivingProcesses.size()) {
+                // while (time < 1117) {
+                    printCycles();
+                    doBlockedProcesses(i);
+                    doRunningProcesses(i);
+                    doArrivingProcesses(i);
+                    doReadyProcesses(i);
+                    time++;
+                }
+            
+            }
+            case 3: {
+               
+                
+                
+                
+
+        
+                while (terminatedProcesses.size() != numOfProcesses) {
+                    doBlockedProcesses(i);
+                    doRunningProcesses(i);
+                    doArrivingProcesses(i);
+                    doReadyProcesses(i);
+                    time++;
+                }
+            }
         }
+        
+       
+
     }
 
     public static void doReadyProcesses(int i) {
+        switch (i) { // 1 for FCFS, 2 for RR with quantum 2, 3 for LCFS, 4 for HPRN.
+            case 1: {     
+                if (RunningProcess == null && readyProcesses.size() > 0) {
+                    RunningProcess = readyProcesses.get(0);
+                    
+                    
+                    int temp = randomOS(RunningProcess.b);
+                    if (temp > RunningProcess.remainingTime) {
+                        RunningProcess.CPUBurst = RunningProcess.remainingTime;
+                    }
+                    else {
+                        RunningProcess.CPUBurst = temp;
+                    }
+                    
+                    RunningProcess.state = "Running";
+                    RunningProcess.remainingBurstTime = RunningProcess.CPUBurst;
+                    RunningProcess.outOfRunning = time + RunningProcess.CPUBurst;
+                    readyProcesses.remove(0);
 
+                }
+    
+                    
+                    for (Process p : readyProcesses) {
+                        p.remainingBurstTime = 0;
+                        p.waitingTime++;
+                    }
+                
+                
+            }
+
+            case 3: {
+                if (RunningProcess == null && readyProcesses.size() > 0) {
+                    Collections.sort(readyProcesses, 
+                    (o1, o2) -> o1.compareTo(o2));
+                    
+                    RunningProcess = readyProcesses.get(readyProcesses.size()-1);
+                    
+                    RunningProcess.CPUBurst = randomOS(RunningProcess.getB());
+                    RunningProcess.remainingBurstTime = RunningProcess.CPUBurst;
+                    RunningProcess.state = "Running";
+                    RunningProcess.outOfRunning = time + RunningProcess.CPUBurst;
+                    RunningProcess.waitingTime += (time  - RunningProcess.outOfBlocked);
+                    readyProcesses.remove(readyProcesses.size()-1);
+                }
+                
+            }
+            default: 
+        }
     }
 
     public static void doRunningProcesses(int i) {
+        if (RunningProcess != null) {
+            if (RunningProcess.outOfRunning == time) {
+                RunningProcess.IOBurst = RunningProcess.CPUBurst * RunningProcess.m;
+                RunningProcess.outOfBlocked = RunningProcess.IOBurst + time;
+                RunningProcess.state = "Blocked";
+                RunningProcess.remainingBurstTime = RunningProcess.IOBurst;
+                RunningProcess.remainingTime -= RunningProcess.CPUBurst;
+                // System.out.println("Running Process Remaining Time: " + RunningProcess.remainingTime);
+                if (RunningProcess.remainingTime <= 0) {
+                    RunningProcess.finishingTime = time;
+                    RunningProcess.state = "Terminated";
+                    RunningProcess.remainingBurstTime = 0;
+                    terminatedProcesses.add(RunningProcess);
+                    RunningProcess = null;
+                }
+                else {
+                    blockedProcesses.add(RunningProcess);
+                    RunningProcess = null;
+                }
+               
+            }
+            else RunningProcess.remainingBurstTime--;
+            
+            
+            
 
+        }
     }
 
     public static void doBlockedProcesses(int i) { //check if out_of_block == current_time where out_of_block = current_time + io_burst 
+        switch (i) {
+            case 1:
+            case 3: {
+                Iterator<Process> iter = blockedProcesses.iterator();
+                removedFromBlockCounter = 0;
+                while (iter.hasNext()) {
+                    Process p = iter.next();
+                    if (p.outOfBlocked == time) {
+                        p.state = "Ready";
+                        p.totalIOTime += p.IOBurst;
+                        readyProcesses.add(p);
+                        iter.remove();
+                        removedFromBlockCounter++;
+                    }
+                    else {
+                        p.remainingBurstTime--;
+                    }
+                }
+                // if (removedFromBlockCounter > 1) {
+                //     if (readyProcesses.get(readyProcesses.size()-1).priority > readyProcesses.get(readyProcesses.size()-2).priority) {
+                //         Collections.swap(readyProcesses, readyProcesses.size()-1, readyProcesses.size()-2);
+                //     }
+                // }
+                // for (Process p : blockedProcesses) {
+                //     if (p.outOfBlocked == time) {
+                //         p.state = "Ready";
+                //         readyProcesses.add(p);
+                //         blockedProcesses.remove(p);
+
+                        
+                //     }
+                // }
+            }
+            default: ;
+        }
         
     }
 
     public static void doArrivingProcesses(int i) {
         for (Process p : arrivingProcesses) {
             if (p.a == time) {
+                p.state = "Ready";
                 readyProcesses.add(p);
+                
             }
+            
         }
     }
+
+    // public static void sortList(ArrayList<Process> arrivingProcesses) {
+    //     Process temp;
+    //     for (int i = 0; i < arrivingProcesses.size(); i++) {
+    //         for (int j = 0; j < arrivingProcesses.size(); j++) {
+    //             if (arrivingProcesses.get(j).a < arrivingProcesses.get(j+1).a) {
+    //                 temp = arrivingProcesses.get(j);
+    //                 arrivingProcesses.set(j, arrivingProcesses.get(j+1));
+    //                 arrivingProcesses.set(j+1, temp);
+    //             }
+    //         }
+            
+    //     }
+    // }
+    public static int randomOS(int u) {
+        
+        int random = randomNumList.get(randomCounter);
+        random = 1 + (random%u);
+        randomCounter++;
+        return random;
+        
+    }
+
+    public static void printCycles() {
+        // String pString = "Before cycle " + time + ":";
+        System.out.print("Before cycle ");
+        System.out.printf("%4d:", time);
+        for (int i = 0; i < arrivingProcesses.size(); i++) {
+            System.out.printf("%-11s", arrivingProcesses.get(i).state);
+            System.out.printf("%-3d", arrivingProcesses.get(i).remainingBurstTime);
+            // pString += "    " + arrivingProcesses.get(i).state + "     " + arrivingProcesses.get(i).remainingBurstTime;
+        }
+        System.out.println("");
+        
+    }
+   // System.out.println("Running Process Remaining Time: " + RunningProcess.remainingTime);
+    
 }
